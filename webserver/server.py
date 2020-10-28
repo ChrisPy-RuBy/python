7cf2c792eb9c512aadc1d7bf9d5860cc131952b4
Chris woodall <christopherhwoodall@gmail.com>
2020-10-28 23:12:47 +0000

    using a socket server

--- webserver/server.py
+++ webserver/server.py
@@ -1,23 +1,47 @@
-import socket
+"""
+What do am I trying to do?
+- don't hang up have 1 connection. Using socket server instead.
+- parse the request and mirror it into the output
 
-HOST = ''
-PORT = 8888
-response = "HTTP/1.1 200 OK\n"\
-           "Content-Type: text/html\n"\
-           "\n"\
-           "<html><body>Hello World</body></html>\n"
+"""
+
+
+
+
+import socketserver
+from http.server import BaseHTTPRequestHandler
 
 another_response = "HTTP/1.1 200 OK\r\nContent-Length: 13\r\nConnection: close\r\n\r\nHello, world!"
 
-with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
-    print(f"serving on: {PORT}")
-    s.bind((HOST, PORT))
-    s.listen(1)
-    conn, addr = s.accept()
-    with conn:
-        print("Connected by", addr)
-        while True:
-            data = conn.recv(1024)
-            if not data: break
-            import pdb; pdb.set_trace()
-            conn.send(another_response.encode())
+class MyTCPHandler(socketserver.BaseRequestHandler):
+    """
+    The request handler class for our server.
+
+    It is instantiated once per connection to the server, and must
+    override the handle() method to implement communication to the
+    client.
+    """
+    counter = 0
+
+
+    def handle(self):
+        # self.request is the TCP socket connected to the client
+        self.data = self.request.recv(1024).strip()
+        print("{} wrote:".format(self.client_address[0]))
+        print(self.data)
+        # just send back the same data, but upper-cased
+        # self.request.sendall(self.data.upper())
+        bin_counter = " {}".format(self.counter)
+        self.request.sendall("".join([another_response, bin_counter]).encode())
+        self.counter += 1
+
+
+if __name__ == "__main__":
+    HOST, PORT = "localhost", 8888
+
+    # Create the server, binding to localhost on port 9999
+    with socketserver.TCPServer((HOST, PORT), MyTCPHandler) as server:
+        # Activate the server; this will keep running until you
+        # interrupt the program with Ctrl-C
+        server.serve_forever()
+C
